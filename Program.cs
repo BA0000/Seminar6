6eb298812645837d5600778787982bdb30eda616
Gunter <kaiz@yandex.ru>
2022-07-18 19:25:01 +0300

    seminar 6 end

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+// Console.Write("Enter quantity of numbers: ");
+
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] newArray = new int[m];
+
+
+// void CreateArray(int m)
+
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         Console.Write($"Enter {i+1} number ");
+//         newArray[i] = Convert.ToInt32(Console.ReadLine());
+        
+//     }
+
+// }
+
+// int CheckNumbers(int[] newArray)
+// {
+//     int count = 0;
+//     for (int i = 0; i < newArray.Length; i++)
+//     {
+//         if (newArray[i] > 0) count += 1;
+//     }
+//     return count;
+// }
+
+// CreateArray(m);
+
+// Console.WriteLine("Numbers more then 0 is: " + CheckNumbers(newArray));
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями x = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+// void CrossPoint(int b1, int k1, int b2, int k2)
+// {
+//   int x = (b1-b2)/(k2-k1);
+//   int y = (k2*b1-k1*b2)/(k2-k1);
+//   if (k1==k2)
+//      Console.Write("Parallel lines");
+//   else 
+//      Console.Write($"point of intersection of straight lines {x} , {y} ");
+
+// }
+
+
+// Console.Write("Enter b1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter k1: ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter b2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter k2: ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+
+
+// CrossPoint(b1, k1, b2, k2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
